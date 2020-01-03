59cdb1b62671e05894e6c667380f52b15e853f96
ikale1234 <55333848+ikale1234@users.noreply.github.com>
2020-01-02 17:02:21 -0800

    game update
    game

---
+++ project.py
@@ -0,0 +1,38 @@
+let = ['.',',','!','?','-','+']
+sent = input()
+listsent = str.split(sent, ' ')
+alist = []
+for i in range(len(listsent)):
+    blist = []
+    w=len(listsent[i])
+    punct = 0
+    for o in range(w):
+        if listsent[i][o] in let:
+            punct = 1
+
+    for j in range(w):
+        if punct == 1:
+            if listsent[i][j] not in let:
+                blist.append(listsent[i][w-2-j])
+            else:
+                blist.append(listsent[i][w-1])
+        else:
+            blist.append(listsent[i][w-1-j])
+    alist.append(blist)
+clist = []
+for i in range(len(alist)):
+    clist.append(''.join(alist[i]))
+done = ' '.join(clist)
+print (done)
+dlist = []
+for i in range(len(listsent)):
+    word = listsent[i]
+    if word not in dlist:
+        dlist.append(word)
+    else:
+        break
+    wcount = 0
+    for i in range(len(listsent)):
+        if listsent[i] == word:
+            wcount+=1
+    print(word+' '+str(wcount))
